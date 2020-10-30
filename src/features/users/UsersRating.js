21f6926d7c96bf063e3bfdc704ca29f45fe42890
artif1c1al <zhuravlenkoartem@gmail.com>
2020-10-30 18:18:56 +0300

    add fancy icons

--- src/features/users/UsersRating.js
+++ src/features/users/UsersRating.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 export default function UsersRating() {
   const posts = useSelector((state) => state.posts)
   const users = useSelector((state) => state.users)
 
+  const reactionEmoji = {
+    thumbsUp: '👍',
+    hooray: '🎉',
+    heart: '❤️',
+    rocket: '🚀',
+    eyes: '👀',
+  }
+
+  const [selected, setSelected] = useState('heart')
+
+  const onSelectedChange = (e) => setSelected(e.target.value)
+
   const rating = posts.map((el, i) => {
     return (
       <h2 key={i}>
         {users[el.user].name}:{' '}
-        <span style={{ color: '#0fa' }}>{el.reactions.heart}</span> likes
+        <span style={{ color: '#0fa' }}>{el.reactions[selected]}</span>{' '}
+        {reactionEmoji[selected]}
       </h2>
     )
   })
 
   return (
-    <div className="usersRating">
+    <section className="usersRating">
       <h1>Rating list:</h1>
+      <label htmlFor="postAuthor">Author:</label>
+      <select id="postAuthor" value={selected} onChange={onSelectedChange}>
+        <option>thumbsUp</option>
+        <option>hooray</option>
+        <option>heart</option>
+        <option>rocket</option>
+        <option>eyes</option>
+      </select>
       <div className="">{rating}</div>
-    </div>
+    </section>
   )
 }
